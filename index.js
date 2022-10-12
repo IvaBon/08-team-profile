0ae795f3022b65679e3bf4d402570b84e2012a5e
ivan bonilla <ivanbonilla02@yahoo.com>
2022-10-12 18:32:38 -0500

    working on question

--- index.js
+++ index.js
@@ -5,20 +5,106 @@ const Engineer=require('./lib/engineer');
 const Intern=require('./lib/intern');
 const generaterFile=require('./src/generateFile');
 
-inquirer
+
+
+function members(){
+   inquirer
     .prompt([
         {
-            type:'list',
-            name:'addWorker',
-            message:'What type of employee do you want to add?',
-            choices:['Manager','Engineer','Intern','Done'],
+            type:'input',
+            name:'name',
+            message:'Enter team member name',
         },
+        {
+            type:'input',
+            name:'id',
+            message:'Enter team member id',
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'Enter team member email',
+        },
+        {
+            type:'list',
+            name:'role',
+            message:'Enter team member role',
+            choices:['Manager','Engineer','Intern'],
+        },
+        
     ])
-    .then((data)=>{
-        switch(data.addWorker){
-            case 'Manager':
-                
+    .then ((data)=>{
+        if (data.role==='Manager'){
+             inquirer.prompt([
+                {
+                    type:'input',
+                    name:'officeNumber',
+                    message:'Enter manager office number',
+                },
+                {
+                    type:'confirm',
+                    name:'again',
+                    message:'Do you want to add another member',
+                    validate: again => {
+                        if(data.again==='Yes'){
+                            members();
+                        }   
+                          
+                        
+                    }
+
+                    
+                    
+                    
+                },
+            ]);
+            
         }
-    });
+        if(data.role==='Engineer'){
+            inquirer.prompt([
+                {
+                    type:'input',
+                    name:'githubUser',
+                    message:'Enter engineer github username',
+                },
+                {
+                    type:'confirm',
+                    name:'again',
+                    message:'Do you want to add another member',
+                },
+            ]);
+        }
+        if(data.role==='Intern'){
+            inquirer.prompt([
+                {
+                    type:'input',
+                    name:'school',
+                    message:'Enter intern school',
+                },
+                {
+                    type:'confirm',
+                    name:'again',
+                    message:'Do you want to add another member',
+                },
+            ]);
+        }
+        
+    })
+//     .then(()=>{
+//         inquirer.prompt([
+//             {
+//                 type:'confirm',
+//                 name:'again',
+//                 message:'Do you want to add another member',
+                
+//             }
+//         ])
+//     })
+  
+}
 
 
+
+
+
+members();
